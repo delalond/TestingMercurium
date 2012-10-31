d233449c588e4a6672acd6b218f39ef781d676b0
Fabien Delalondre <fabien.delalondre@epfl.ch>
2012-10-31 14:17:32 +0100

    Adding dummy main to test mercurium compilers on JSC judge (intel Xeon) infrastructure

---
+++ main_MPI.c
@@ -0,0 +1,8 @@
+
+#include "mpi.h"
+
+int main(int argc, char* argv)
+{
+
+  return 0;
+}
---
+++ main_MPI.cxx
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "mpi.h"
+using namespace std;
+int  main()
+{
+  cout << "Hello World!" << endl;   cout << "Welcome to C++ Programming" << endl; 
+  return 0;
+}
